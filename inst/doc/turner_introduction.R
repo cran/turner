2864a59e9e437d207989758ed8cb03c44e83e926
Frederic Bertrand <frederic.bertrand@utt.fr>
2024-04-04 08:00:02 +0000

    version 0.1.9

--- inst/doc/turner_introduction.R
+++ inst/doc/turner_introduction.R
@@ -1,10 +1,8 @@
-
-## @knitr load_turner, message=FALSE
+## ----load_turner, message=FALSE-----------------------------------------------
 # load package turner
 library(turner)
 
-
-## @knitr data_matrix
+## ----data_matrix--------------------------------------------------------------
 # create a matrix
 set.seed = 21
 some_data = round(matrix(rnorm(90), 10, 9), 3)
@@ -14,26 +12,22 @@ colnames(some_data) = paste("X", 1:9, sep='')
 # take a peek
 head(some_data, n=5)
 
-
-## @knitr blocks
+## ----blocks-------------------------------------------------------------------
 # list of blocks
 blocks = list(B1 = 1:3, B2 = 4:5, B3 = 6:9)
 blocks
 
-
-## @knitr indexify
+## ----indexify-----------------------------------------------------------------
 # get indices of blocks
 indices = indexify(blocks)
 indices
 
-
-## @knitr list_to_dummy
+## ----list_to_dummy------------------------------------------------------------
 # get dummy matrix based on blocks
 dummy = list_to_dummy(blocks)
 dummy
 
-
-## @knitr from_to
+## ----from_to------------------------------------------------------------------
 # get starting and ending positions
 start_end = from_to(blocks)
 start_end
@@ -42,28 +36,23 @@ start_end
 from = start_end$from
 to = start_end$to
 
-
-## @knitr extract_first_block
+## ----extract_first_block------------------------------------------------------
 # extract first block
 some_data[,from[1]:to[1]]
 
-
-## @knitr get_first_block
+## ----get_first_block----------------------------------------------------------
 # get first block
 some_data[,blocks[[1]]]
 
-
-## @knitr str_list
+## ----str_list-----------------------------------------------------------------
 # string list
 str_list = list(c("a","b","c"), c("d", "e"), c("f","g","h","i"))
 
+## ----failed_extraction, eval=FALSE--------------------------------------------
+#  # failed attempt
+#  some_data[,str_list[[1]]]
 
-## @knitr failed_extraction, eval=FALSE
-## # failed attempt
-## some_data[,str_list[[1]]]
-
-
-## @knitr success_extraction
+## ----success_extraction-------------------------------------------------------
 # start-end position for 'str_list'
 fromto_aux = from_to(str_list)
 from1 = fromto_aux$from
@@ -72,8 +61,7 @@ to1 = fromto_aux$to
 # successful attempt
 some_data[,from1[1]:to1[1]]
 
-
-## @knitr lenghts
+## ----lenghts------------------------------------------------------------------
 # say you have some list
 some_list = list(1:3, 4:5, 6:9)
 
@@ -83,13 +71,11 @@ lengths(some_list, out='vector')
 # length of each vector (list output)
 lengths(some_list, out='list')
 
-
-## @knitr length
+## ----length-------------------------------------------------------------------
 # compared to 'length()'
 length(some_list)
 
-
-## @knitr funlist
+## ----funlist------------------------------------------------------------------
 # sum of all elements in 'some_list'
 funlist(some_list, sum)
 
@@ -102,17 +88,14 @@ funlist(some_list, prod)
 # mean value of all elements in 'some_list'
 funlist(some_list, mean)
 
-
-## @knitr listsize
+## ----listsize-----------------------------------------------------------------
 # number of elements in 'some_list'
 listsize(some_list)
 
-
-## @knitr listify
+## ----listify------------------------------------------------------------------
 # vector of indices
 number_elements = c(3, 1, 5)
 
 # list of index vectors based on 'number_elements'
 listify(number_elements)
-
 
